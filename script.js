d1845d5c5c4608b3fe98a75e5bf33cebcc243e69
SPNicolas <nicolassteffenspereira@gmail.com>
2023-12-08 09:15:37 -0300

    feito

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function inserir(num) {
     document.getElementById("display").innerHTML += num;//Ele serve para inserir um número ou operador.
 }
@@ -17,3 +18,52 @@ function total() {
         document.querySelector(".display").innerHTML = "Comando errado";
     }
 }
+
+document.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        calcular()
+    }
+    if (event.key == "1") {
+        inserir(1)
+    } 
+    if (event.key == "2") {
+        inserir(2)
+    } 
+    if (event.key == "3") {
+        inserir(3)
+    } 
+    if (event.key == "4") {
+        inserir(4)
+    } 
+    if (event.key == "5") {
+        inserir(5)
+    } 
+    if (event.key == "6") {
+        inserir(6)
+    } 
+    if (event.key == "7") {
+        inserir(7)
+    } 
+    if (event.key == "8") {
+        inserir(8)
+    } 
+    if (event.key == "9") {
+        inserir(9)
+    } 
+    if (event.key == "+") {
+        inserir('+')
+    }
+    if (event.key == "-") {
+        inserir("-")
+    }
+    if (event.key == "/") {
+        inserir("/")
+    }
+    if (event.key == "c") {
+        limpar()
+    }
+    if (event.key == ".") {
+        inserir(".")
+    }
+        
+})
